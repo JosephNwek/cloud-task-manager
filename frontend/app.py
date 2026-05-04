5846fb02f6294f0b69e3a4948eb80bb5e2b6d4b2
JosephNwek <131289072+JosephNwek@users.noreply.github.com>
2026-05-03 23:17:00 -0400

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -17,4 +17,4 @@ def add_task():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
